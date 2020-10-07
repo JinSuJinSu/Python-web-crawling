9feb3d9ea6e58c691b63d9655c82acc0f1907f0f
hjs429 <hjs928@naver.com>
2020-10-07 22:50:05 +0900

    refactor: Change function names to check easily

--- Calculator.py
+++ Calculator.py
@@ -8,38 +8,38 @@ root.geometry("640x480+500+200")#가로x세로 + x좌표 +y좌표
 
 
 #계산기 기능을 위한 변수
-num_input = StringVar()
+btn_input = StringVar()
 expression = ''
-# 버튼 기능 넣기
-def btn_click(num):
+# 클릭 기능 넣기
+def btn_click(btn):
     global expression
-    expression = expression + str(num)
-    num_input.set(expression)
+    expression = expression + str(btn)
+    btn_input.set(expression)
 
 
 # Clear 기능 넣기
-def num_init():
+def btn_clear():
     pass
 
 # 나누기 기능 넣기
-def num_div():
+def btn_div():
     pass
 
 # 곱하기 기능 넣기
-def num_mul():
+def btn_mul():
     pass
 
 # 더하기 기능 넣기
-def num_plu():
+def btn_add():
     pass
 
 # 빼기 기능 넣기
-def num_sub():
+def btn_sub():
     pass
 
 
 # 계산된 숫자가 쓰일 화면을 만든다.
-screen_width = Entry(root, width=20, textvariable = num_input)
+screen_width = Entry(root, width=20, textvariable = btn_input)
 screen_width.grid(row = 0, columnspan = 4, sticky = N+E+W+S, padx = 3, pady = 3, ipady = 5)
 
 #맨 윗줄부터 차례대로 계산기에 들어가는 버튼들을 추가해준다.
@@ -48,7 +48,7 @@ screen_width.grid(row = 0, columnspan = 4, sticky = N+E+W+S, padx = 3, pady = 3,
 btn_left_parentheses= Button(root, text = '(', width =5, height =2, command=lambda:btn_click('('))
 btn_right_parentheses= Button(root, text = ')', width =5, height =2, command=lambda:btn_click(')'))
 btn_percent= Button(root, text = '%', width =5, height =2, command=lambda:btn_click('%'))
-btn_AC= Button(root, text = 'Clear', width =5, height =2, command = num_init)
+btn_AC= Button(root, text = 'Clear', width =5, height =2, command = btn_clear)
 
 
 btn_left_parentheses.grid(row = 1, column = 0, sticky = N+E+W+S, padx = 3, pady = 3)
@@ -61,7 +61,7 @@ btn_AC.grid(row = 1, column = 3, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_7= Button(root, text = '7', width =5, height =2, command=lambda:btn_click(7))
 btn_8= Button(root, text = '8', width =5, height =2, command=lambda:btn_click(8))
 btn_9= Button(root, text = '9', width =5, height =2, command=lambda:btn_click(9))
-btn_division= Button(root, text = '/', width =5, height =2, command = num_div)
+btn_division= Button(root, text = '/', width =5, height =2, command = btn_div)
 
 btn_7.grid(row = 2, column = 0, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_8.grid(row = 2, column = 1, sticky = N+E+W+S, padx = 3, pady = 3)
@@ -73,7 +73,7 @@ btn_division.grid(row = 2, column = 3, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_4= Button(root, text = '4', width =5, height =2, command=lambda:btn_click(4))
 btn_5= Button(root, text = '5', width =5, height =2, command=lambda:btn_click(5))
 btn_6= Button(root, text = '6', width =5, height =2, command=lambda:btn_click(6))
-btn_multiplication= Button(root, text = 'x', width =5, height =2, command = num_mul)
+btn_multiplication= Button(root, text = 'x', width =5, height =2, command = btn_mul)
 
 btn_4.grid(row = 3, column = 0, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_5.grid(row = 3, column = 1, sticky = N+E+W+S, padx = 3, pady = 3)
@@ -84,7 +84,7 @@ btn_multiplication.grid(row = 3, column = 3, sticky = N+E+W+S, padx = 3, pady = 
 btn_1= Button(root, text = '1', width =5, height =2, command=lambda:btn_click(1))
 btn_2= Button(root, text = '2', width =5, height =2, command=lambda:btn_click(2))
 btn_3= Button(root, text = '3', width =5, height =2, command=lambda:btn_click(3))
-btn_subtraction= Button(root, text = '-', width =5, height =2, command = num_sub)
+btn_subtraction= Button(root, text = '-', width =5, height =2, command = btn_sub)
 
 btn_1.grid(row = 4, column = 0, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_2.grid(row = 4, column = 1, sticky = N+E+W+S, padx = 3, pady = 3)
@@ -95,12 +95,12 @@ btn_subtraction.grid(row = 4, column = 3, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_0= Button(root, text = '0', width =5, height =2, command=lambda:btn_click(0))
 btn_dot= Button(root, text = '.', width =5, height =2, command=lambda:btn_click('.'))
 btn_equal= Button(root, text = '=', width =5, height =2)
-btn_plus= Button(root, text = '+', width =5, height =2, command = num_plu)
+btn_adds= Button(root, text = '+', width =5, height =2, command = btn_add)
 
 btn_0.grid(row = 5, column = 0, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_dot.grid(row = 5, column = 1, sticky = N+E+W+S, padx = 3, pady = 3)
 btn_equal.grid(row = 5, column = 2, sticky = N+E+W+S, padx = 3, pady = 3)
-btn_plus.grid(row = 5, column = 3, sticky = N+E+W+S, padx = 3, pady = 3)
+btn_adds.grid(row = 5, column = 3, sticky = N+E+W+S, padx = 3, pady = 3)
 
 
 
